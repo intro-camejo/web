1effeff89a379f425bab2609f3943de151a0ded5
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-16 20:10:05 -0300

    Remove .docusaurus from git

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/manu/Documents/Facultad/Intro/web/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/manu/Documents/Facultad/Intro/web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/manu/Documents/Facultad/Intro/web/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/manu/Documents/Facultad/Intro/web/src/css/custom.css'),
-];
