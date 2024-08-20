608c4551b61a8300abcaccbbf40830250d15cb1a
RiedelNicolas <nariedel99@gmail.com>
2024-08-20 01:18:03 -0300

    feature multiples imagenes

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
@@ -9,75 +9,92 @@ import NicoImg from '@site/static/img/nico.jpg';
 import GonzaImg from '@site/static/img/gonza.jpeg';
 import EmmaImg from '@site/static/img/emma.jpeg'
 import ManuBImg from '@site/static/img/manub.jpg';
-import ManuCImg from '@site/static/img/manuc.jpg';
+import ManuCImg from '@site/static/img/manu1.jpeg';
+import ManuC2Img from '@site/static/img/manu2.jpeg';
+import ManuC3Img from '@site/static/img/image.png';
 import ArielImg from '@site/static/img/ariel.jpeg';
 import NicoHImg from '@site/static/img/nicoh.png';
 import SofiImg from '@site/static/img/sofi.jpeg';
+import Sofi2Img from '@site/static/img/sofi2.jpeg';
 import ConraIMG from '@site/static/img/conra.jpeg';
 import PedroIMG from '@site/static/img/pedroimg.jpeg';
 import LaraIMG from '@site/static/img/lara.jpeg';
+import Nico2 from '@site/static/img/nico2.jpg';
 
 type ColabItem = {
   name : string,
-  picture : string
+  pictures : string[]
 };
 
 const ColabList: ColabItem[] = [
   {
     name: 'Peke',
-    picture: PekeImg,
+    pictures: [PekeImg],
   },
   {
     name: 'Nico R',
-    picture: NicoImg,
+    pictures: [NicoImg, Nico2],
   },
   {
     name: 'Gonza',
-    picture: GonzaImg,
+    pictures: [GonzaImg],
   },
   {
     name: 'Emma',
-    picture: EmmaImg,
+    pictures: [EmmaImg],
   },
   {
     name: 'Manu B',
-    picture: ManuBImg,
+    pictures: [ManuBImg],
   },
   {
     name: 'Manu C',
-    picture: ManuCImg,
+    pictures: [ManuCImg, ManuC2Img, ManuC3Img],
   },
   {
     name: 'Ariel',
-    picture: ArielImg,
+    pictures: [ArielImg],
   },
   {
     name: 'Nico H',
-    picture: NicoHImg,
+    pictures: [NicoHImg],
   },
   {
     name: 'Sofi',
-    picture: SofiImg,
+    pictures: [SofiImg,Sofi2Img],
   },
   {
     name: 'Conra',
-    picture : ConraIMG
+    pictures : [ConraIMG]
   },
   {
     name: 'Pedro',
-    picture : PedroIMG
+    pictures : [PedroIMG]
   },
   {
     name: 'Lara',
-    picture : LaraIMG
+    pictures : [LaraIMG]
   }
 ];
 
-function Colab({ name, picture }: ColabItem) {
+function Colab({ name, pictures }: ColabItem) {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  
+  const handleClick = () => {
+    setCurrentImage((prev) => (prev + 1) % pictures.length);
+  };
+  
   return (
     <div className={clsx('col col--3')}>
       <div className="text--center">
-          <img className={styles.featureIMG} alt="Foto del colaborador" src={picture}/>
+        <img
+          className={styles.featureIMG}
+          alt="Foto del colaborador"
+          src={pictures[currentImage]}
+          onClick={handleClick}
+          style={{ cursor: pictures.length > 1 ? 'pointer' : 'default' }}
+        />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{name}</h3>
